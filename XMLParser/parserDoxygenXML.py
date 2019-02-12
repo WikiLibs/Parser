83a59bee8ea9edc257c45d9a59aade4b358fa579
samuel <samuel.rousseaux@epitech.eu>
2019-02-12 01:21:01 +0100

    Added file path

--- XMLParser/parserDoxygenXML.py
+++ XMLParser/parserDoxygenXML.py
@@ -4,10 +4,22 @@ from sys import argv as av
 
 from src.getDefine import getDefine
 
+def getFilePath(root):
+    try:
+        path = root.find("compounddef").find("incdepgraph").find("node").find("label").text
+    except:
+        try:
+            path = root.find("compounddef").find("compoundname").text
+        except:
+            path = "none"
+    return path
+
 def main():
     defines = []
     root = ET.parse(av[1]).getroot()
 
+    path = getFilePath(root)
+    print(path)
     for elem in root.iter('memberdef'):
         if elem.get('kind') == 'define':
             defines = getDefine(elem, defines)
