eeb5509587cfa8583fbe21a5ac8a0bef1ca9213b
vik <chau.victor.yves@gmail.com>
2019-06-05 15:00:45 +0200

    [92] now python and C parser is completely distinct with unit test
    JSONRequestCrafter no more require the lang to parse

---
+++ Parser/Tests/test_jsonRequestCrafter.py
@@ -0,0 +1,37 @@
+import unittest
+
+import sys
+import io
+
+import Parser.useful as useful
+import Parser.jsonRequestCrafter as jsonRequestCrafter
+
+
+class Test_StrOperations(unittest.TestCase):
+    def test_distinc_wip(self):
+        '''
+        it should print WIP
+        '''
+        capturedOutput = io.StringIO()  # setup an io
+        sys.stdout = capturedOutput  # redirect stdout
+        list = ['test']
+        list2 = [('client', list), ('client', list)]
+        jsonRequestCrafter.JSONRequestCrafter('C', 'Test', list2)
+        sys.stdout = sys.__stdout__  # reset stdout
+        self.assertEqual(capturedOutput.getvalue(), 'This feature is in WIP\n', 'Should print \'This feature is in WIP\'')
+
+    def test_distinc_error(self):
+        '''
+        it should print WIP
+        '''
+        with self.assertRaises(SystemExit) as cm:
+            capturedOutput = io.StringIO()  # setup an io
+            sys.stdout = capturedOutput  # redirect stdout
+            list = ['test']
+            list2 = [('client', list), ('error', list)]
+            jsonRequestCrafter.JSONRequestCrafter('C', 'Test', list2)
+            sys.stdout = sys.__stdout__  # reset stdout
+
+        self.assertEqual(cm.exception.code, 1)  # assert exit code
+        self.assertEqual(capturedOutput.getvalue(), 
+        (useful.RED + useful.BOLD + "[ERROR]" + useful.RESET + " - key error not found in JSONRequestCrafter (line:220)\n" + useful.BOLD + "[---Exiting program---]" + useful.RESET + "\n"), "Should print the expected")
