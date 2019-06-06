1be0445b03b83e069243e89d6e4d1f997b2420b6
vik <chau.victor.yves@gmail.com>
2019-06-06 23:20:47 +0200

    [92] test build crash pipeline 9

--- DevOps/requirements.sh
+++ DevOps/requirements.sh
@@ -4,6 +4,7 @@ pip3 install pyinstaller
 pip3 install pytest
 pip3 install pytest-cov
 pip3 install doxypypy
+pip3 install requests
 
 OS=`awk -F= '$1=="ID" { print $2 ;}' /etc/os-release`
 if [ $OS = "ubuntu" ] || [ $OS = "fedora" ]; then
--- Parser/aiClient.py
+++ Parser/aiClient.py
@@ -1,4 +1,4 @@
-import request
+import requests
 import json
 import useful
 
@@ -20,7 +20,7 @@ class AIClient:
             "email": "wikilibs@yuristudio.net",
             "password": "wikilibs-parser"
         }
-        res = request.post(API_URL + "/auth/internal/login", headers=headers, json=loginJson)
+        res = requests.post(API_URL + "/auth/internal/login", headers=headers, json=loginJson)
         if (res.status_code != 200):
             raise ConnectionError("Could not obtain authorization token")
 
@@ -31,7 +31,7 @@ class AIClient:
         headers = {
             "Authorization": "Bearer " + token
         }
-        res = request.post(API_URL + "/symbol", headers=headers, json=y)
+        res = requests.post(API_URL + "/symbol", headers=headers, json=y)
         if (res.status_code != 200):
             raise IOError(res.text)
 
@@ -43,7 +43,7 @@ class AIClient:
             "email": "wikilibs@yuristudio.net",
             "password": "wikilibs-parser"
         }
-        res = request.post(API_URL + "/auth/internal/login", headers=headers, json=loginJson)
+        res = requests.post(API_URL + "/auth/internal/login", headers=headers, json=loginJson)
         if (res.status_code != 200):
             raise ConnectionError("Could not obtain authorization token")
 
@@ -54,6 +54,6 @@ class AIClient:
         headers = {
             "Authorization": "Bearer " + token
         }
-        res = request.patch(API_URL + "/symbol/optimize", headers=headers)
+        res = requests.patch(API_URL + "/symbol/optimize", headers=headers)
         if (res.status_code != 200):
             raise IOError(res.text)
