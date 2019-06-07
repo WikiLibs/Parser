4343fad9124310fbd18c954e8cae575be9a9feff
vik <chau.victor.yves@gmail.com>
2019-06-07 20:49:58 +0200

    [91] utest 2

--- Parser/Tests/test_aiClient.py
+++ Parser/Tests/test_aiClient.py
@@ -21,7 +21,9 @@ class Test_aiClient(unittest.TestCase):
     res2.text = "test"
 
     @patch('requests.post', return_value=res)
+    @patch('Parser.aiClient.AIClient.GetToken', return_value="test")
     def test_push_symbol_error(self,
+                               mock_get_token,
                                mock_request_post):
         sym = jsClass.SymbolUpdate("test")
         sym.setLang("test2")
@@ -49,13 +51,12 @@ class Test_aiClient(unittest.TestCase):
         client.GetToken()
 
     @patch('requests.patch', return_value=res)
+    @patch('Parser.aiClient.AIClient.GetToken', return_value="test")
     def test_optimize_error_patch(self,
+                                  mock_get_token,
                                   mock_request_patch):
-        with self.assertRaises(OSError) as cm:
-            capturedOutput = io.StringIO()  # setup an io
-            sys.stdout = capturedOutput  # redirect stdout
-            client = aiClient.AIClient()
-            client.CallOptimizer()
+        client = aiClient.AIClient()
+        client.CallOptimizer()
 
     @patch('requests.post', return_value=res2)
     def test_get_token_no_error(self,
