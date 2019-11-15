8daaa9422b3804ae44fe9d74a3784c3809535124
vik <chau.victor.yves@gmail.com>
2019-11-16 00:13:28 +0900

    [228] Fix API connection IDs

--- Parser/aiClient.py
+++ Parser/aiClient.py
@@ -6,9 +6,9 @@ import useful
 # WikiLibs AI Client python module
 
 API_URL = "https://wikilibs-dev-api.azurewebsites.net"
-APP_KEY = "5f0a4531-a612-4ca7-8b22-9aeaa12fcf9e"
-APP_ID = "6429a514-89c9-4296-9c51-407a1986c824"
-SEC = "oQ5,{nH5*àfA3.^lQ7%}aE2_"
+APP_KEY = "39d183b2-955f-4d28-90d6-b711e3db355b" # ne pas changer
+APP_ID = "96abd6ae-a122-49c0-abbb-278d65d53531"
+SEC = "mY3#èvF2,<xT4-çuB6!}nQ8."
 
 
 class AIClient:
@@ -29,9 +29,11 @@ class AIClient:
             "appId": APP_ID,
             "appSecret": SEC
         }
+        print("API key: #" + self._apikey + "#")
         res = requests.post(API_URL + "/auth/bot", headers=headers, json=loginJson)
         if (res.status_code != 200):
-            raise ConnectionError("Authorization token rejected")
+            raise ConnectionError("Authorization token rejected " + str(res.status_code) + "\n"
+                                  + res.text)
 
         # Extract bearer token string
         self._token = res.text[1:-1]
