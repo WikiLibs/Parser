f642f98bb0adec3f4d4edadf56f4aa2a0d200915
Yuri Edward <nicolas1.fraysse@epitech.eu>
2020-02-10 10:58:01 +0100

    Added class parser

--- Parser/Lang_CPP/parseClass.py
+++ Parser/Lang_CPP/parseClass.py
@@ -1,10 +1,17 @@
 import Parser.getters as getters
+from Parser.genericClasses import buildPrototype
+from Parser.genericClasses import buildParameter
+from Parser.genericClasses import buildClass
+from classes import variableClass
+from Parser.Lang_CPP.parserCPP import kindTable
 
 def parseClass(root):
+    #Obtain generic information about the class
     name = root.find("compoundname").text
     path = name.replace("::", "/")
     classProto = cpdef.get("prot") + " "
     cpdef = root.find("compounddef")
+    importStr = cpdef.find("includes").text
     isAbstract = cpdef.get("abstract")
     if (isAbstract == "yes"):
         classProto = classProto + "abstract "
@@ -14,15 +21,41 @@ def parseClass(root):
     description = briefDesc
     if (len(detailedDesc) > 0):
         description = detailedDesc
-    # TODO : INSERT HERE > retrieve parameters from template parameter list
-    #params = getters.getParamDesc(root, )
-    # TODO : INSERT HERE > Add parameters to some prototype parameter list
-    # TODO : INSERT HERE > Parse class members
+    templatePars = []
+    tlist = cpdef.find("templateparamlist")
+    for elem in tlist.iter('param'):
+        v = variableClass()
+        v.type = elem.get("type")
+        v.name = elem.get("declname")
+        v.value = elem.get("defval")
+        templatePars.append(v)
+    if (len(templatePars) > 0):
+        prefix = "template <"
+        for v in templatePars:
+            prefix = prefix + v.type + " " + v.name
+            if (tdef != None):
+                prefix = prefix + " = " + v.value
+            prefix = prefix + ", "
+        prefix = prefix[:2]
+        classProto = prefix + classProto
+        templatePars = getters.getParamDesc(cpdef, templatePars)
 
-
-    #for elem in root.iter('memberdef'):
-    #    kind = elem.get('kind')
-    #    print(kind)
-
-    #for elem in root.iter('innerclass'):
-    #    refid = elem.get('refid')
+    #Build a symbol for the class
+    proto = buildPrototype(prototype=classProto, description=description)
+    for v in templatePars:
+        proto.addParameter(buildParameter(prototype=v.type + " " + v.name, description=v.desc))
+    cl = buildClass(path=path, prototypeObj=proto, importString=importStr)
+    for elem in root.iter('memberdef'):
+        kind = elem.get('kind')
+        path = path + "/" + elem.find("name").text
+        cl.addMember(path)
+        syms = []
+        if (kind in kindTable):
+            syms = kindTable[kind](elem)
+        for s in syms:
+            s.path = path + "/" + s.path
+            self.appendToSymbols("generic", s)
+    for elem in root.iter('innerclass'):
+        path = elem.text.replace("::", "/")
+        cl.addMember(path)
+    self.appendToSymbols("generic", cl)
