9752ba37f4ff1dc5d36051e68993986bb37d3bd5
vik <chau.victor.yves@gmail.com>
2020-05-20 19:04:28 +0900

    [249] refactor Java parser

--- Parser/Lang_Java/getVariable.py
+++ Parser/Lang_Java/getVariable.py
@@ -1,11 +1,25 @@
 from classes import variableClass
+from genericClasses import buildVariable
+from genericClasses import buildPrototype
 import getters as getters
 
 
-def getVariable(elem):
-    tmpVariable = variableClass()
+# def getVariableOld(elem):
+#     tmpVariable = variableClass()
 
-    tmpVariable.name = getters.getName(elem)
-    tmpVariable.type = getters.getType(elem)
-    tmpVariable.value = getters.getInitializer(elem)
-    return tmpVariable
+#     tmpVariable.name = getters.getName(elem)
+#     tmpVariable.type = getters.getType(elem)
+#     tmpVariable.value = getters.getInitializer(elem)
+#     return tmpVariable
+
+def getVariable(elem):
+    syms = []
+    name = getters.getName(elem)
+    include = getters.getLocation(elem)
+    vtype = getters.getType(elem)
+    value = getters.getInitializer(elem)
+    briefDesc = getters.getBriefDesc(elem)
+
+    varProto = buildPrototype(vtype + " " + name, briefDesc)
+    syms.append(buildVariable(path=name, prototypeObj=varProto, importString=include))
+    return syms
--- Parser/Lang_Java/parserJava.py
+++ Parser/Lang_Java/parserJava.py
@@ -1,34 +1,56 @@
 #!/usr/bin/env python3.6
 import os
 import xml.etree.ElementTree as ET
-from Lang_Python.getFunction import getFunction
+from Lang_Java.getFunction import getFunction
 from Lang_Java.getClass import getClass
 from Lang_Java.getVariable import getVariable
 from languageInterface import LanguageInterface
 
-
+kindTable = {
+    "variable": getVariable,
+    "function": getFunction,
+    "class": getClass
+}
 
 class parserJava(LanguageInterface):
+    # def getSymbolsOld(self, filename):
+    #     newFilename = filename[6:-8]
+    #     namespaceFilename = './xml/namespace' + newFilename + '.xml'
+
+    #     classFilenames = getClassesFiles(newFilename)
+
+    #     if os.path.isfile(namespaceFilename):
+    #         namespaceRoot = ET.parse(namespaceFilename).getroot()
+
+    #         for elem in namespaceRoot.iter('membderdef'):
+    #             kind = elem.get('kind')
+    #             if kind == 'variable':
+    #                 super().appendToSymbols('variable', getVariable(elem))
+    #             if kind == 'function':
+    #                 super().appendToSymbols('function', getFunction(elem))
+
+    #     for classFile in classFilenames:
+    #         classFileRoot = ET.parse(classFile).getroot()
+    #         super().appendToSymbols('class', getClass(classFileRoot))
+
     def getSymbols(self, filename):
         newFilename = filename[6:-8]
         namespaceFilename = './xml/namespace' + newFilename + '.xml'
 
         classFilenames = getClassesFiles(newFilename)
-
+        syms = []
         if os.path.isfile(namespaceFilename):
             namespaceRoot = ET.parse(namespaceFilename).getroot()
-
-            for elem in namespaceRoot.iter('membderdef'):
+            for elem in namespaceRoot.iter('memberdef'):
                 kind = elem.get('kind')
-                if kind == 'variable':
-                    super().appendToSymbols('variable', getVariable(elem))
-                if kind == 'function':
-                    super().appendToSymbols('function', getFunction(elem))
+                syms += kindTable[kind](elem)
 
         for classFile in classFilenames:
             classFileRoot = ET.parse(classFile).getroot()
-            super().appendToSymbols('class', getClass(classFileRoot))
+            syms += kindTable['class'](classFileRoot)
 
+        for s in syms:
+            self.appendToSymbols("generic", s)
 
 def getClassesFiles(filename):
     result = os.popen('find . -name class*' + filename + '*.xml -print').read().split()
