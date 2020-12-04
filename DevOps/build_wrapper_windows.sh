4cbf0da9618980fc25a4bcf97dfb57dbac0c0387
Yuri Edward <yuri6037@outlook.com>
2020-12-04 10:29:41 +0100

    Fixed bad binary name for windows

--- DevOps/build_wrapper_windows.sh
+++ DevOps/build_wrapper_windows.sh
@@ -3,5 +3,5 @@ cd Wrapper-C/Windows/mybins
 ../bin2c.exe -b ../../../wikilibs_parser.exe mybin
 cd ../..
 ./run.sh
-cp build/Windows/Wrapper_Windows ../wikilibs_parser_wrapper_windows
+cp build/Windows/Wrapper_Windows.exe ../wikilibs_parser_wrapper_windows.exe
 cd ..
